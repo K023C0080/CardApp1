c574543374145afac6f2d4e1b889dd6b0965aa05
K023C0080 <K023C0080@g.neec.ac.jp>
2025-01-16 14:14:45 +0900

    komiito

--- Game.java
+++ Game.java
@@ -1,3 +1,26 @@
 public class Game {
-  
+  Player p;
+  Card[] cards;
+
+Game() {
+  p = new Player();
+
+  cards = new Card[52];
+  String[] marks = {"♠", "♡", "♦", "♧"};
+  for (int i=0 ; i<52 ; i++){
+    Card c = new Card();
+    cards[i] = c;
+  }
+}
+
+
+
+  void start(){
+    Card c = p.turn(cards);
+    c.open();
+    judge(c);
+  }
+  void judge(Card c){
+    if 
+  }
 }
